2a5c6011246b26901f9b227135ab3b7692ed384f
bryce <brycekbargar@gmail.com>
2016-05-23 09:41:59 -0500

    Setting up view tests

--- test/filter.specs.js
+++ test/filter.specs.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
+const jsdomify = require('jsdomify').default;
 
 const Controller = require('../source/components/filter/filter.controller.js');
 
@@ -9,5 +10,15 @@ describe('For the controller', () => {
         let controller = new Controller();
 
         expect(controller.filters()).to.be.have.length(2);
+    });
+});
+
+describe('For the views', () => {
+    before('create the dom', () => jsdomify.create());
+    beforeEach('clear the dom', () => jsdomify.clear());
+    after('destroy the dom', () => jsdomify.destroy());
+
+    describe('expect FilterView', () => {
+        it('should be ok', () => expect(true).to.be.ok );
     });
 });
