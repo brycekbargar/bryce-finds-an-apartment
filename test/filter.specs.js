46525649b62b9687e6a3d6cd0cb629b0598c87ec
bryce <brycekbargar@gmail.com>
2016-05-23 19:26:23 -0500

    Test the single filter view

--- test/filter.specs.js
+++ test/filter.specs.js
@@ -3,7 +3,10 @@
 const expect = require('chai').expect;
 const jsdomify = require('jsdomify').default;
 
+const m = require('mithril');
+const Filter = require('../source/models/filter.js');
 const Controller = require('../source/components/filter/filter.controller.js');
+const views = require('../source/components/filter/filter.view.js');
 
 describe('For the controller', () => {
     it('expect it to have two testing filters', () => {
@@ -14,11 +17,43 @@ describe('For the controller', () => {
 });
 
 describe('For the views', () => {
-    before('create the dom', () => jsdomify.create());
-    beforeEach('clear the dom', () => jsdomify.clear());
+    before('create the dom', () => jsdomify.create('<html><body></body></html>'));
     after('destroy the dom', () => jsdomify.destroy());
+    beforeEach('jsdom setup', () => {
+        jsdomify.clear();
+        this.document = jsdomify.getDocument();
+        m.deps(this.document.defaultView);
+    });
 
-    describe('expect FilterView', () => {
-        it('should be ok', () => expect(true).to.be.ok );
+    describe('expect FilterView to', () => {
+        beforeEach('setup filter', () => this.filter = new Filter({name: 'Test Filter'}));
+        beforeEach('render view', () => {
+            m.mount(
+                this.document.body, 
+                {
+                    view: () => views.filterView(null, [this.filter, 1])
+                });
+        });
+        it('have an id', () => {
+            let element = this.document.getElementById('filter1');
+            expect(element).to.not.be.null;
+        });
+        it('be disabled by default', () => {
+            let element = this.document.getElementById('filter1');
+            expect(element.checked).to.be.false;
+        });
+        it('have checks update the model', () => {
+            let element = this.document.getElementById('filter1');
+            let label = this.document.getElementsByTagName('label')[0];
+            element.checked = true;
+            label.click();
+            expect(this.filter.enabled()).to.be.true;
+        });
+        it('be bound to the model', () => {
+            this.filter.enabled(true);
+            m.redraw(true);
+            let element = this.document.getElementById('filter1');
+            expect(element.checked).to.be.true;
+        });
     });
 });
