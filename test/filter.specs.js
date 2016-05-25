8fd31a9e8e31f92165073cba94060cb2f56ea588
bryce <brycekbargar@gmail.com>
2016-05-25 09:39:55 -0500

    Nest the tests by component

--- test/filter.specs.js
+++ test/filter.specs.js
@@ -5,46 +5,49 @@ const jsdomify = require('jsdomify').default;
 
 const m = require('mithril');
 const Filter = require('../source/models/filter.js');
-const FilterView = require('../source/components/filter/view.js');
+const View = require('../source/components/filter/view.js');
 
-describe('For the FilterView expect it to', () => {
-    before('create the dom', () => jsdomify.create('<html><body></body></html>'));
-    after('destroy the dom', () => jsdomify.destroy());
-    beforeEach('jsdom setup', () => {
-        jsdomify.clear();
-        this.document = jsdomify.getDocument();
-        m.deps(this.document.defaultView);
-    });
-    beforeEach('setup filter', () => this.filter = new Filter({name: 'Test Filter'}));
-    beforeEach('render view', () => {
-        m.mount(this.document.body, {
-            view: () => FilterView(null, [this.filter, 1])
+describe('For the FilterComponent', () => {
+
+    describe('expect the View to', () => {
+        before('create the dom', () => jsdomify.create('<html><body></body></html>'));
+        after('destroy the dom', () => jsdomify.destroy());
+        beforeEach('jsdom setup', () => {
+            jsdomify.clear();
+            this.document = jsdomify.getDocument();
+            m.deps(this.document.defaultView);
+        });
+        beforeEach('setup filter', () => this.filter = new Filter({name: 'Test Filter'}));
+        beforeEach('render view', () => {
+            m.mount(this.document.body, {
+                view: () => View(null, [this.filter, 1])
+            });
+        });
+        it('have an id', () => {
+            let element = this.document.getElementById('filter1');
+            expect(element).to.not.be.null;
+        });
+        it('have a label', () => {
+            let element = this.document.getElementsByTagName('label')[0];
+            expect(element.textContent).to.equal('Test Filter');
+        });
+        it('be disabled by default', () => {
+            let element = this.document.getElementById('filter1');
+            expect(element.checked).to.be.false;
+        });
+        it('have checks update the model', () => {
+            let element = this.document.getElementById('filter1');
+            let label = this.document.getElementsByTagName('label')[0];
+            element.checked = true;
+            label.click();
+            expect(this.filter.enabled()).to.be.true;
+        });
+        it('be bound to the model', () => {
+            this.filter.enabled(true);
+            m.redraw(true);
+            let element = this.document.getElementById('filter1');
+            expect(element.checked).to.be.true;
         });
     });
 
-    it('have an id', () => {
-        let element = this.document.getElementById('filter1');
-        expect(element).to.not.be.null;
-    });
-    it('have a label', () => {
-        let element = this.document.getElementsByTagName('label')[0];
-        expect(element.textContent).to.equal('Test Filter');
-    });
-    it('be disabled by default', () => {
-        let element = this.document.getElementById('filter1');
-        expect(element.checked).to.be.false;
-    });
-    it('have checks update the model', () => {
-        let element = this.document.getElementById('filter1');
-        let label = this.document.getElementsByTagName('label')[0];
-        element.checked = true;
-        label.click();
-        expect(this.filter.enabled()).to.be.true;
-    });
-    it('be bound to the model', () => {
-        this.filter.enabled(true);
-        m.redraw(true);
-        let element = this.document.getElementById('filter1');
-        expect(element.checked).to.be.true;
-    });
 });
--- test/filterBox.specs.js
+++ test/filterBox.specs.js
@@ -5,39 +5,42 @@ const jsdomify = require('jsdomify').default;
 
 const m = require('mithril');
 const Filter = require('../source/models/filter.js');
-const FilterBoxController = require('../source/components/filterBox/controller.js');
-const FilterBoxView = require('../source/components/filterBox/view.js');
+const Controller = require('../source/components/filterBox/controller.js');
+const View = require('../source/components/filterBox/view.js');
 
-describe('For the controller', () => {
-    it('expect it to have two testing filters', () => {
-        let controller = new FilterBoxController();
-        expect(controller.filters()).to.be.have.length(2);
-    });
-});
+describe('For the FilterBoxComponent', () => {
 
-describe('For the FilterBoxView expect it to', () => {
-    before('create the dom', () => jsdomify.create('<html><body></body></html>'));
-    after('destroy the dom', () => jsdomify.destroy());
-    beforeEach('jsdom setup', () => {
-        jsdomify.clear();
-        this.document = jsdomify.getDocument();
-        m.deps(this.document.defaultView);
+    describe('expect the Controller to', () => {
+        it('have two testing filters', () => {
+            let controller = new Controller();
+            expect(controller.filters()).to.be.have.length(2);
+        });
     });
-    beforeEach('setup filter', () => this.filter = new Filter({name: 'Test Filter'}));
-    beforeEach('render view', () => {
-        let filters = [
-            new Filter({name: 'First'}),
-            new Filter({name: 'Second'}),
-            new Filter({name: 'Third'})
-        ];
-        m.mount(this.document.body, {
-            controller: function(){ return {filters: () => filters}; },
-            view: FilterBoxView 
+    
+    describe('expect the View to', () => {
+        before('create the dom', () => jsdomify.create('<html><body></body></html>'));
+        after('destroy the dom', () => jsdomify.destroy());
+        beforeEach('jsdom setup', () => {
+            jsdomify.clear();
+            this.document = jsdomify.getDocument();
+            m.deps(this.document.defaultView);
+        });
+        beforeEach('setup filter', () => this.filter = new Filter({name: 'Test Filter'}));
+        beforeEach('render view', () => {
+            let filters = [
+                new Filter({name: 'First'}),
+                new Filter({name: 'Second'}),
+                new Filter({name: 'Third'})
+            ];
+            m.mount(this.document.body, {
+                controller: function(){ return {filters: () => filters}; },
+                view: View 
+            });
+        });
+        it('render many filters', () => {
+            let filters = this.document.getElementsByTagName('span'); 
+            expect(filters).to.have.length(3);
         });
     });
 
-    it('render many filters', () => {
-        let filters = this.document.getElementsByTagName('span'); 
-        expect(filters).to.have.length(3);
-    });
 });
