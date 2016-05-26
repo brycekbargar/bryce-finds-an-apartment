c0c245f3ea9dd73b569f317aed542727bfeb865c
bryce <brycekbargar@gmail.com>
2016-05-25 22:21:56 -0500

    Load google map

--- source/components/map/map.view.js
+++ source/components/map/map.view.js
@@ -1,6 +1,18 @@
 const m = require('mithril');
+const GoogleMapsLoader = require('google-maps');
 
-let map = function(){
+GoogleMapsLoader.CLIENT = process.env.GOOGLE_MAPS_CLIENT_KEY;
+GoogleMapsLoader.LIBRARIES = ['drawing', 'geometry', 'places'];
+const mapOptions = {
+};
+
+let map = function(element, isInit, context){
+    if(isInit){
+        GoogleMapsLoader.load(
+            (google) => 
+                context.map = new google.maps.Map(element, mapOptions));
+        context.onunload(() => GoogleMapsLoader.release());
+    }
 };
 
 module.exports = () => m('div#mapContainer', [m('div#map', { config: map })]);
