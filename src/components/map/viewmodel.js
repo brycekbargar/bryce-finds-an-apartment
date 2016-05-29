a7a7f47884792ef6821b4191702f9f0382decbb0
bryce <brycekbargar@gmail.com>
2016-05-29 13:24:49 -0500

    Cache radarSearch results

--- src/components/map/viewmodel.js
+++ src/components/map/viewmodel.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Promise = require('bluebird');
+const ls = require('local-storage');
 
 const MapFilter = function(filter){
     let circles = [];
@@ -20,6 +21,10 @@ const MapFilter = function(filter){
 
     this.init = (places, newCircle) => 
         new Promise((resolve, reject) => {
+            if(ls(filter.name())) {
+                resolve(ls(filter.name()));
+                return;
+            }
             places.radarSearch({
                 location: {lat: 41.881832, lng: -87.623177},
                 radius: 16000,
@@ -28,23 +33,25 @@ const MapFilter = function(filter){
             },
             (radarResults, status) => {
                 if(status === 'OK') {
-                    radarResults.map((r) =>
-                        circles.push(newCircle({
-                            center: r.geometry.location,
-                            radius: filter.radius(),
-                            visible: filter.enabled(),
-                            fillColor: filter.color(),
-                            fillOpacity: .35,
-                            strokeWeight: 0
-                        })));
-                    resolve();
+                    ls(filter.name(), radarResults);
+                    resolve(radarResults);
+                    return;
                 }
                 else {
                     reject(new Error(status));
                 }
             });
-        });
-
+        })
+        .then((radarResults) => 
+            radarResults.map((r) =>
+                circles.push(newCircle({
+                    center: r.geometry.location,
+                    radius: filter.radius(),
+                    visible: filter.enabled(),
+                    fillColor: filter.color(),
+                    fillOpacity: .35,
+                    strokeWeight: 0
+                }))));
 };
 
 module.exports = function(filters){
