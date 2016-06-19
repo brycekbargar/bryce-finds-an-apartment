5d4cf23e3188cf59453a20834222cf9eca67ea69
bryce <brycekbargar@gmail.com>
2016-06-19 16:11:30 -0500

    Remove the hard-coded directions objects

--- src/components/directionsBox/view.js
+++ src/components/directionsBox/view.js
@@ -7,5 +7,24 @@ module.exports = (ctrl) =>
     m('div.DirectionsBox.u-flexbox.u-flexbox--vertical.u-box', {
         config: ctrl.vm.getDirections
     }, [
-        ctrl.vm.directions().map((d) => m(DirectionsComponent, {directions: d}))
+        ctrl.vm.directions().map((d) => m(DirectionsComponent, {directions: d})),
+        m('span.u-flexbox.u-flexbox--vertical', [
+            m('span.DirectionsBox--name', [
+                m('label[for=Name]', 'Name:' ),
+                m('input#Name[type=text]', {
+                    onchange: m.withAttr('value', ctrl.vm.name),
+                    value: ctrl.vm.name()
+                })
+            ]),
+            m('span.DirectionsBox--address', [
+                m('label[for=Address]', 'Address:' ),
+                m('input#Address[type=text]', {
+                    onchange: m.withAttr('value', ctrl.vm.address),
+                    value: ctrl.vm.address()
+                })
+            ]),
+            m('button[type=button]', {
+                onclick: ctrl.vm.add
+            }, 'Add')
+        ])
     ]);
--- src/components/directionsBox/viewmodel.js
+++ src/components/directionsBox/viewmodel.js
@@ -7,7 +7,18 @@ const Promise = require('bluebird');
 module.exports = function(google) {
     let vm = this;
     vm.google = google;
-    vm.directions = m.prop();
+    vm.directions = m.prop([]);
+
+    vm.name = m.prop('');
+    vm.address = m.prop('');
+    vm.add = () => {
+        vm.directions().push(new Directions({
+            name: vm.name(),
+            address: vm.address()
+        }));
+        vm.name('');
+        vm.address('');
+    };
 
     let previousLocation;
     vm.getDirections = () => {
@@ -62,31 +73,4 @@ module.exports = function(google) {
             .finally(() => m.endComputation());
         }
     };
-
-    vm.directions([
-        new Directions({
-            name: 'Lessons',
-            address: '2217 W Ferdinand St, Chicago, IL 60612'
-        }),
-        new Directions({
-            name: 'Therapy',
-            address: '980 N Michigan Ave, Chicago, IL 60611'
-        }),
-        new Directions({
-            name: 'Movie Night',
-            address: '651 S Wells St, Chicago, IL 60607'
-        }),
-        new Directions({
-            name: 'SSH',
-            address: '1048 W 37th St #105, Chicago, IL 60609'
-        }),
-        new Directions({
-            name: 'CBC',
-            address: '1907 N Mendell St #4C, Chicago, IL 60642'
-        }),
-        new Directions({
-            name: 'BKB',
-            address: '100 S Morgan St, Chicago, IL 60607'
-        })
-    ]);
 };
